7b6620e3b3b450c79f68f5a182493520c675484f
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-06-18 13:56:03 -0400

    zoom in and text

--- js/home.js
+++ js/home.js
@@ -1,56 +1,6 @@
-/*
-var robotCanvas = document.getElementById("robotAnimationCanvas")
-var robotDiv = document.getElementById("robotAnimationDiv")
-var state = {
-    imageTickCount : 2,
-    prevY : robotDiv.getBoundingClientRect().y
-}
-
-robotCanvas.width = window.innerWidth/2
-robotCanvas.height = window.innerHeight
-
-var context = robotCanvas.getContext("2d");
-context.fillStyle = "rgb(0,0,0)";
-context.fillRect(0, 0, robotCanvas.width, robotCanvas.height);
-context.stroke()
-
-var img = new Image()
-img.src = `/img/spinny_boi/${"1".toString().padStart(4, "0")}.png`
-console.log(img.src)
-img.onload=function(){
-    context.drawImage(img, 0, 0, 1920/3, 1080/3);
-    context.stroke()
-}
-
-robotDiv.onwheel = imageLoader
-
-function imageLoader(event){
-    console.log(event)
-    console.log(state.imageTickCount, robotDiv.getBoundingClientRect().y)
-
-    if((state.prevY >= 0 && robotDiv.getBoundingClientRect().y <= 0) && state.imageTickCount < 60){
-        if(event.wheelDelta < 0){
-            state.imageTickCount += 1
-        }
-        else {
-            state.imageTickCount -= 1
-        }
-        event.preventDefault();
-        img.src = `/img/spinny_boi/${state.imageTickCount.toString().padStart(4, "0")}.png`
-        context.fillStyle = "rgb(0, 0, 0)"
-        context.fillRect(0, 0, robotCanvas.width, robotCanvas.height)
-        console.log(img.src)
-        context.drawImage(img, 0, 0, 1920/3, 1920/3)
-        
-    }
-    else {
-        state.prevY = robotDiv.getBoundingClientRect().y
-    }
-}
-*/
-
 console.clear();
 
+const html = document.documentElement;
 const canvas = document.getElementById("robotAnimationCanvas");
 const animationSpace = document.getElementById("robotAnimationSpace");
 const context = canvas.getContext("2d");
@@ -58,16 +8,26 @@ const context = canvas.getContext("2d");
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-var imageWidth = window.innerWidth*2/3
-var imageHeight = 1080*imageWidth/1920
+var imageWidth;
+var imageHeight;
+var imageLeft;
+var imageTop;
+var resizing = true
 
-window.addEventListener('resize', function() {
-    console.log("resized")
+function resize(event){
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-    imageWidth = window.innerWidth
-    imageHeight = 1080*window.innerHeight/1920
-});
+    imageWidth = Math.min(window.innerWidth*2/3, 1100)
+    imageHeight = 1080*imageWidth/1920
+    imageLeft = (window.innerWidth-imageWidth)/2
+    imageTop = (window.innerHeight-imageHeight)/2
+    //context.fillStyle = "rgb(0,0,0)";
+    //context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+
+window.addEventListener('resize', resize);
+resize(null)
 
 const frameCount = 120;
 const frameOffset = 25;
@@ -78,7 +38,6 @@ const currentFrame = index => (
 animationSpace.style.height = `${(window.innerHeight*3).toString()}px`
 
 const images = []
-const html = document.documentElement;
 const airpods = {
   frame: 1
 };
@@ -89,12 +48,22 @@ for (let i = 1; i < frameCount; i += 2) {
   images.push(img);
 }
 
+document.addEventListener("scroll", (event) => {
+    if(resizing == true){
+        var multiplier = (window.pageYOffset+500)/(window.innerHeight+500)
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        //context.fillStyle = "rgb(0,0,0)";
+        //context.fillRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(images[0], (window.innerWidth-(imageWidth*multiplier))/2, imageTop*multiplier, imageWidth*multiplier, imageHeight*multiplier); 
+    }
+})
+
 gsap.to(airpods, {
   frame: 59 + frameOffset,
   snap: "frame",
   ease: "none",
   scrollTrigger: {
-    scrub: 0.01
+    scrub: 0.2
   },
   onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
 });
@@ -104,24 +73,33 @@ images[0].onload = render;
 
 function render() {
     if (airpods.frame > frameOffset){
+        resizing = false
         if(animationSpace.getBoundingClientRect().y < 0){
+            console.log("changed to fixed")
             canvas.style.position = "fixed"
             canvas.style.top = '0px'
+            //document.getElementById("robotName").style.position = "fixed"
+            //document.getElementById("robotName").style.top = `96px`
         }
         else if(animationSpace.getBoundingClientRect().y > 0){
             canvas.style.position = "relative"
+            //document.getElementById("robotName").style.position = "relative"
         }
         try {
             console.log(images[airpods.frame-frameOffset].src)
-            context.drawImage(images[airpods.frame-frameOffset], 0, window.innerHeight/4, imageWidth, imageHeight); 
+            context.drawImage(images[airpods.frame-frameOffset], imageLeft, imageTop, imageWidth, imageHeight);
             context.clearRect(0, 0, canvas.width, canvas.height);
-            context.fillStyle = "rgb(211,211,211)";
-            context.fillRect(0, 0, canvas.width, canvas.height);
-            context.drawImage(images[airpods.frame-frameOffset], 0, window.innerHeight/4, imageWidth, imageHeight); 
+ 
+            //context.fillStyle = "rgb(0,0,0)";
+            //context.fillRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(images[airpods.frame-frameOffset], imageLeft, imageTop, imageWidth, imageHeight); 
         }
         catch (error) {
             console.log(error)
-            context.drawImage(images[airpods.frame-1-frameOffset], 0, window.innerHeight/4, imageWidth, imageHeight); 
+            context.drawImage(images[airpods.frame-1-frameOffset], imageLeft, imageTop, imageWidth, imageHeight); 
         }
+    }
+    else {
+        resizing = true
     }
 }
